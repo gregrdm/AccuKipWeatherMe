6040d207bcf03c166ea5a06caf2f3e3666a98d78
radomg01 <radomg01@stepstone.com>
2021-05-10 19:24:52 +0200

    Added: empty details fragment with viewModel, viewBinding.

---
+++ app/src/main/java/com/gregrdm/accukipweatherme/ui/details/WeatherDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.gregrdm.accukipweatherme.ui.details
+
+import androidx.lifecycle.ViewModel
+
+class WeatherDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/gregrdm/accukipweatherme/ui/main/MainFragment.kt
+++ app/src/main/java/com/gregrdm/accukipweatherme/ui/main/MainFragment.kt
@@ -1,31 +1,51 @@
 package com.gregrdm.accukipweatherme.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.gregrdm.accukipweatherme.R
+import com.gregrdm.accukipweatherme.databinding.MainFragmentBinding
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.main_fragment) {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+
+        val cityInputText = binding.inputCityName.editText?.text.toString()
+        binding.inputCityName.editText?.doAfterTextChanged {
+            if (it?.trim().toString() != cityInputText) {
+                // todo call viewModel.fetchCities()
+                Log.d("CityField", "Changed value to: ${it.toString()}")
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    companion object {
+        fun newInstance() = MainFragment()
     }
 }
